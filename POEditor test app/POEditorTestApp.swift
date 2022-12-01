acd9019dcad11a5265d8b2a06b6299aa0e74df0f
Daniel Lisovoy <daniel.lisovoy@gmail.com>
2022-12-01 15:54:37 +0100

    first commit

---
+++ POEditor test app/POEditorTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  poeditor_testApp.swift
+//  poeditor test
+//
+//  Created by Daniel Lisovoy on 01.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct POEditorTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
